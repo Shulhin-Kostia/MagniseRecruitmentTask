0d55f291faa3feb31513e4ba1f9bd7f8f487ddf6
Shulhin-Kostia <kavshulhd@gmail.com>
2024-05-26 11:33:26 +0300

    POST supported coins removed
    Temporarily removed endpoint for supported coins addition. This is not the main feature and it requires role-based authorization implemented.

--- MagniseRecruitmentTask/Program.cs
+++ MagniseRecruitmentTask/Program.cs
@@ -33,28 +33,28 @@ app.MapGet("/coins/supported", async (AppDbContext context) =>
     return Results.Ok(await context.Coins.Select(c => c.Code).ToListAsync());
 });
 
-app.MapPost("/coins/supported", async (CoinApiService coinApiService, AppDbContext context, [FromBody, Required] string coinCode) =>
-{
-    if(!Regex.IsMatch(coinCode, @"^[A-Z]{3,5}$"))
-        return Results.BadRequest("Invalid format of coin code. You should use only from 3 to 5 uppercase letters.");
-    if (context.Coins.Any(c => c.Code.Equals(coinCode)))
-        return Results.BadRequest("Coin already added.");
+//app.MapPost("/coins/supported", async (CoinApiService coinApiService, AppDbContext context, [FromBody, Required] string coinCode) =>
+//{
+//    if(!Regex.IsMatch(coinCode, @"^[A-Z]{3,5}$"))
+//        return Results.BadRequest("Invalid format of coin code. You should use only from 3 to 5 uppercase letters.");
+//    if (context.Coins.Any(c => c.Code.Equals(coinCode)))
+//        return Results.BadRequest("Coin already added.");
 
-    var exchangeRate = await coinApiService.GetRateAsync(coinCode);
-    if (exchangeRate == default)
-        return Results.BadRequest("Such coin doesn't exist.");
+//    var exchangeRate = await coinApiService.GetRateAsync(coinCode);
+//    if (exchangeRate == default)
+//        return Results.BadRequest("Such coin doesn't exist.");
 
-    context.Coins.Add(
-        new Coin()
-        {
-            Code = coinCode,
-            Price = exchangeRate!.rate,
-            PriceUpdated = exchangeRate!.time
-        });
-    await context.SaveChangesAsync();
+//    context.Coins.Add(
+//        new Coin()
+//        {
+//            Code = coinCode,
+//            Price = exchangeRate!.rate,
+//            PriceUpdated = exchangeRate!.time
+//        });
+//    await context.SaveChangesAsync();
 
-    return Results.Ok(await context.Coins.FirstAsync(c => c.Code.Equals(coinCode)));
-});
+//    return Results.Ok(await context.Coins.FirstAsync(c => c.Code.Equals(coinCode)));
+//});
 
 app.MapGet("/coins", async (AppDbContext context, [FromQuery] string ids) =>
 {
